893e9200685c2dde7c7180995264affa79cc30a9
Gerar-do <211228@ids.upchiapas.edu.mx>
2023-11-27 15:21:37 -0600

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const PORT = process.env.PORT || 4000;
+export const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb+srv://211228:39Cq6DWa2FpmYf8g@cluster0.rasynuu.mongodb.net/monitoreo?retryWrites=true&w=majority";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://127.0.0.1:5173";
